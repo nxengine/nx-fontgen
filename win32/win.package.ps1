18258ba692c7c7a598e94a12afd2a25563ac6841
iSage <isage.dna@gmail.com>
2019-04-07 23:08:53 +0300

    Win release build

---
+++ win32/win.package.ps1
@@ -0,0 +1,31 @@
+#prepare
+
+mkdir release
+cd release
+
+#prepare and copy data
+if ($env:PLATFORM -eq "x86") {
+  cp ..\win32\fontbm\Release\fontbm.exe .\fontbm.exe
+} else {
+  cp ..\x64\fontbm\Release\fontbm.exe .\fontbm.exe
+}
+
+if ($env:PLATFORM -eq "x86") {
+  #bundle libs
+  cp ..\win32\ext\runtime\x86\* .\
+
+  #create zip
+  $name = "fontbm-$env:APPVEYOR_REPO_TAG_NAME-Win32.zip"
+  7z a $name .\
+
+  Push-AppveyorArtifact $name
+} else {
+  #bundle libs
+  cp ..\win32\ext\runtime\x64\* .\
+
+  #create zip
+  $name = "fontbm-$env:APPVEYOR_REPO_TAG_NAME-Win64.zip"
+  7z a $name .\
+
+  Push-AppveyorArtifact $name
+}
